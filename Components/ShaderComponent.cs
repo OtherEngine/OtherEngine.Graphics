4c2ba194ecbdd7f200c937885342daf46aad925c
copygirl <halfnerd.copygirl@gmail.com>
2015-09-01 22:05:06 +0200

    Add ShaderController

---
+++ Components/ShaderComponent.cs
@@ -0,0 +1,48 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OtherEngine.Core;
+
+namespace OtherEngine.Graphics.Components
+{
+	public class ShaderComponent : Component
+	{
+		/// <summary>
+		/// Gets the ID handle of the shader.
+		/// </summary>
+		public int ID { get; private set; }
+
+		/// <summary>
+		/// Gets the type of the shader.
+		/// </summary>
+		// TODO: Use OpenTK's enum or a custom one?
+		public ShaderType Type { get; private set; }
+
+
+		/// <summary>
+		/// Gets whether the shader has been
+		/// successfully compiled and can be used.
+		/// </summary>
+		public bool Compiled { get; internal set; }
+
+		/// <summary>
+		/// Gets the source string of the shader.
+		/// May be null if it hasn't been set yet.
+		/// (Though what would you do with an empty shader?)
+		/// </summary>
+		public string Source { get; internal set; }
+
+		/// <summary>
+		/// Gets the information log for the most recent
+		/// compilation of the shader. May be null if the
+		/// shader has not been compiled yet.
+		/// </summary>
+		public string InfoLog { get; internal set; }
+
+
+		internal ShaderComponent(int id, ShaderType type)
+		{
+			ID = id;
+			Type = type;
+		}
+	}
+}
+
---
+++ Controllers/ShaderController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL4;
+using OtherEngine.Core;
+using OtherEngine.Core.Attributes;
+using OtherEngine.Core.Components;
+using OtherEngine.Core.Events;
+using OtherEngine.Core.Tracking;
+using OtherEngine.Graphics.Components;
+
+namespace OtherEngine.Graphics.Controllers
+{
+	[AutoEnable]
+	public class ShaderController : Controller
+	{
+		[TrackComponent]
+		public EntityCollection<TextureComponent> Shaders { get; private set; }
+
+
+		public EntityRef<ShaderComponent> Create(ShaderType type)
+		{
+			var handle = GL.CreateShader(type);
+
+			return new Entity(Game) { new TypeComponent { Value = "Shader" } }
+				.AddRef(new ShaderComponent(handle, type));
+		}
+
+		public void SetSource(EntityRef<ShaderComponent> shader, string source)
+		{
+			GL.ShaderSource(shader.Component.ID, source);
+			shader.Component.Source = source;
+		}
+
+		public void Compile(EntityRef<ShaderComponent> shader)
+		{
+			GL.CompileShader(shader.Component.ID);
+
+			int compileStatus;
+			GL.GetShader(shader.Component.ID, ShaderParameter.CompileStatus, out compileStatus);
+
+			shader.Component.Compiled = (compileStatus > 0);
+			shader.Component.InfoLog = GL.GetShaderInfoLog(shader.Component.ID);
+		}
+
+		public void Delete(EntityRef<ShaderComponent> shader)
+		{
+			shader.Entity.Remove(shader.Component);
+		}
+
+
+		public EntityRef<ShaderComponent> CreateAndCompile(ShaderType type, string source)
+		{
+			var shader = Create(type);
+			SetSource(shader, source);
+			Compile(shader);
+			return shader;
+		}
+
+
+		[SubscribeEvent]
+		void OnShaderComponentRemoved(ComponentRemovedEvent<ShaderComponent> ev)
+		{
+			GL.DeleteShader(ev.Component.ID);
+		}
+	}
+}
+
